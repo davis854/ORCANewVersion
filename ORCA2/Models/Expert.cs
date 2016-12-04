e34b3de16d3424586dd58682821b8b9c8bbe87bf
davis854 <davis854@marshall.edu>
2016-12-04 17:06:08 -0500

    This is a test of final version

---
+++ ORCA2/Models/Expert.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ORCA2.Models
+{
+    public class Expert
+    {
+        [Key]
+        public int ExpertID { get; set; }//44444
+        public int ExpertiseID { get; set; }
+        public Boolean Validated { get; set; }
+    }
+}
